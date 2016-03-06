11b0e07176e9976e513fc3824c764546c8986260
John Polarinakis <John Polarinakis>
2016-03-06 17:06:27 -0600

    added javadoc

--- src/assignment4/Assign4Driver.java
+++ src/assignment4/Assign4Driver.java
@@ -17,8 +17,8 @@ public class Assign4Driver {
 	private static List<String> inputs;
 
 	public static void main(String[] args) throws Throwable {
-		WordLadderSolver word = new WordLadderSolver();
-		InitializeInputs(args);		
+		WordLadderSolver word = new WordLadderSolver(); //driver to control the ladder
+		InitializeInputs(args);		//prepares the map
 		/*
 		 * Begin test code
 		 * */
@@ -37,11 +37,14 @@ public class Assign4Driver {
 		/*
 		 * End test code
 		 * */
-		String firstWord = null;
-		String secondWord = null;
+		/*
+		 * Begins actual code
+		 * */
+		String firstWord = null; //beginning of word ladder
+		String secondWord = null;// end of word ladder
 		for (int i = 0; i < inputs.size(); i++ ) {
 			Scanner scanner = new Scanner(inputs.get(i));
-		
+			//error handling for incorrect number of arguments
 			try {
 				firstWord = scanner.next();
 			} catch (Exception e) {
@@ -56,15 +59,17 @@ public class Assign4Driver {
 				System.out.println("There is no second word");
 				continue;
 			}
+			//computes word ladder in the event that there are inputs
 			try {
 				List<String> result = word.computeLadder(firstWord,
 						secondWord);
-
+				//does not output in the event that it's null
 				if(result == null){}
 				else{
 				for (int x = 0; x < result.size(); x ++) {
 					System.out.println(result.get(x));
 				}
+				//outputs the result
 				System.out.println("********************************************************");
 				System.out.println("The results of the test are: ");
 				boolean correct = word.validateResult(firstWord, secondWord,result);
@@ -82,7 +87,10 @@ public class Assign4Driver {
 
 
 	}
-
+	/*
+	 * prepares the map for the use in the program
+	 * @param String[] args gives the inputed data
+	 * */
 	private static void InitializeInputs(String[] args) {
 		// Check if there is a valid file in the argument
 		if (args.length != 1) {
@@ -94,6 +102,7 @@ public class Assign4Driver {
 		
 		// Open file; file name specified in args (command line)
 		try {
+			//reads valid words out of the dictionary
 			FileReader freader = new FileReader(args[0]);
 			BufferedReader reader = new BufferedReader(freader);
 			for (String s = reader.readLine(); s != null; s = reader.readLine()) {
